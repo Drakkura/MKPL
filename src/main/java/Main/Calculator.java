8c11e06c4b103d566466204a29f7420e85c33fcc
Ryan Piskadinata <91777752+Drakkura@users.noreply.github.com>
2024-06-07 10:20:03 +0700

    inspect gagal

--- src/main/java/Main/Calculator.java
+++ src/main/java/Main/Calculator.java
@@ -50,5 +50,14 @@ public class Calculator {
         }
         return min;
     }
+      public int getMin(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
 }
 
